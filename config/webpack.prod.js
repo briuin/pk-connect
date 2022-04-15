7e158f2d86ab603493230ca7a9bba5953228e51e
Edward Lim <Edward.Lim@redcypress.com.tw>
2022-04-15 14:46:18 +0800

    update wp config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,18 +6,10 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const prodConfig = {
-  entry: {
-    pkConnect: "./src/index.js"
-  },
   mode: 'production',
   output: {
     path: path.resolve(__dirname, '../docs'),
-    filename: '[name].js',
-    chunkFilename: 'lib_[name].js',
     publicPath: 'https://briuin.github.io/pk-connect/'
-  },
-  optimization: {
-    chunkIds: 'named',
   },
   plugins: [
     new CleanWebpackPlugin(),
